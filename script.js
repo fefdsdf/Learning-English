d8b1cadb52d56116401677eb625346ff3767cba5
None <amirrtengelbaev@gmail.com>
2025-07-09 11:46:04 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementById("theme-toggle");
-const body = document.body;
-
-toggle.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  toggle.textContent = body.classList.contains("dark") ? "🌙" : "🌞";
-});
